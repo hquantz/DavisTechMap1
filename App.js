4f8733ea57916ad8cc7752334b892f90199e5640
hquantz <95895131+hquantz@users.noreply.github.com>
2023-06-07 18:00:33 -0600

    update

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { Pressable } from "react-native";
 
 export default function App() {
+  const [selectedLocation, setSelectedLocation] = useState();
+
+  function selectLocationHandler(event) {
+    console.log(event.nativeEvent.coordinate.latitude);
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+
+  const region = {
+    latitude: 41.029493870109484,
+    longitude: -111.92547710963815,
+    latitudeDelta: 0.02,
+    longitudeDelta: 0.01,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <Pressable onPress={selectLocationHandler}>
+        <MapView
+          style={styles.map}
+          initialRegion={region}
+          onPress={selectLocationHandler}
+        >
+          {selectedLocation && (
+            <Marker
+              title="Picked Location"
+              coordinate={{
+                latitude: selectedLocation.lat,
+                longitude: selectedLocation.lng,
+              }}
+              pinColor="red"
+            />
+          )}
+        </MapView>
+      </Pressable>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
